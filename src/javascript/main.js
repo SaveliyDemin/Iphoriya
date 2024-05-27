2ff189d3b6fd2cd9d68d9c9726a7ad427e2c15a4
SaveliyDemin <deminalal@yandex.ru>
2024-05-27 21:49:27 +0400

    1.4

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,14 @@
+const ginbtn1 = "Ежемесяцный платеж";
+const ginbtn2 = "Регестрация";
+const gininput = "Строка поиска";
+const ginmenu = "Меню";
+const ginmenusen = "Настойки";
+const ginmenulig = "Освещение";
+const ginmenurev = "Отзыв";
+const ginaboutcomp = "О компании";
+const ginlan = "Смена языка на Азербайджанский";
+const ginlan1 = "Смена языка на Русский";
+const dopgl = document.querySelector(".dop-gl");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const foothead = document.querySelector(".foothead");
@@ -52,19 +63,53 @@ const rewiewinpval = document.querySelector(".rewiewinpval");
 const reviewlook = document.querySelector(".reviewlook");
 const reviewposlook = document.querySelector(".reviewposlook");
 const afterclickprice = document.querySelector(".afterclickprice");
+const afterclickprice1 = document.querySelector(".afterclickprice1");
 const input = document.querySelector(".input");
 const otmenaprice = document.querySelector(".otmenaprice");
 const container = document.querySelector(".container");
+const btninput = document.querySelector(".btninput");
+const minifoothead = document.querySelector(".minifoothead");
+const main1 = document.querySelector("main");
+const company = document.querySelector(".company");
+const aboutcompany = document.querySelector(".aboutcompany");
+const ktomititle = document.querySelector(".ktomititle");
+const aboutcompanyotmena = document.querySelector(".aboutcompanyotmena");
 
-
-otmenaprice.addEventListener('click', function(){
-    afterclickprice.style = "display:none;";
-    body.style = "background-color: white";
-    foothead.style = "background-color: rgb(252, 252, 252);";
-    header.style = "background-color: rgb(252, 252, 252);";
-    container.style = "display:none;";
+aboutcompanyotmena.addEventListener('click', function(){
+    main1.style = "display:none;";
 });
+company.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginaboutcomp}</h3>
+    `
+    menuafterclick.style = "display:none;";
+    main1.style = "display:block;display: flex;";
+    menu1.style = "display:none;";
+    menu.style = "display:block;display:flex;";
+});
+input.addEventListener('click', function(){
+    btninput.style = "display:block;";
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${gininput}</h3>
+    `
+});
+afterclickprice1.addEventListener('click', function(){
+    btninput.style = "display:none;";
+});
+
+
 btn1.addEventListener('click', function(){
+    // minifoothead = "display:block;";
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginbtn1}</h3>
+    `
+    topbtn.style = "background-color:rgb(49, 48, 45); color:white; ";
+    ktomititle.style = "background-color: whitesmoke;color: #000000;display:flex;";
+    aboutcompany.style = "background-color: whitesmoke;color: black;";
+    main1.style = "display:none;";
     container.style = "display:block;";
     afterclickprice.style = "display:block;";
     body.style = "background-color: #4b55632d;";
@@ -77,6 +122,20 @@ btn1.addEventListener('click', function(){
     menu.style = "display:block;display:flex;";
     menu1.style = "display: none;";
     afterlogin.style = "display:none;";
+    btninput.style = "display:none;";
+    minifoothead.style = "background-color: #4b55632d;";
+});
+otmenaprice.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya"></h3>
+    `
+    afterclickprice.style = "display:none;";
+    body.style = "background-color: white";
+    foothead.style = "background-color: rgb(252, 252, 252);";
+    header.style = "background-color: rgb(252, 252, 252);";
+    container.style = "display:none;";
+    minifoothead.style = "background-color: #fafafa;";
 });
 topbtn.addEventListener('click', tophand );
 // window.addEventListener('scroll', namewindow); 
@@ -101,6 +160,15 @@ function tophand(){
 // }; 
 if (reviewinp.value != "" || reviewinp.value != " " || reviewinp.value != "  " || reviewinp.value != "   "|| reviewinp.value != "    ") {
     review.addEventListener('click', function(){
+        dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginmenu}</h3>
+        <svg class="arrowto9" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  d="M0.739665 0.200408C0.436132 0.48226 0.418556 0.956809 0.700408 1.26034L4.22652 5L0.700408 8.73967C0.418556 9.0432 0.436132 9.51775 0.739665 9.7996C1.0432 10.0815 1.51775 10.0639 1.7996 9.76034L5.7996 5.51034C6.0668 5.22258 6.0668 4.77743 5.7996 4.48966L1.7996 0.239665C1.51775 -0.0638681 1.0432 -0.081444 0.739665 0.200408Z" fill="#323232"></path>
+        </svg>
+        <h3 class="dop-glavnaya1">${ginmenurev}</h3>
+
+    `
         afterclickreview.style = "display:block;";
         menuafterclick.style = "display:none;";
         sentings.style = "display:none;";
@@ -140,14 +208,38 @@ if (reviewinp.value != "" || reviewinp.value != " " || reviewinp.value != "  " |
     
 // });
 btnsentings.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginmenu}</h3>
+        <svg class="arrowto9" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  d="M0.739665 0.200408C0.436132 0.48226 0.418556 0.956809 0.700408 1.26034L4.22652 5L0.700408 8.73967C0.418556 9.0432 0.436132 9.51775 0.739665 9.7996C1.0432 10.0815 1.51775 10.0639 1.7996 9.76034L5.7996 5.51034C6.0668 5.22258 6.0668 4.77743 5.7996 4.48966L1.7996 0.239665C1.51775 -0.0638681 1.0432 -0.081444 0.739665 0.200408Z" fill="#323232"></path>
+        </svg>
+        <h3 class="dop-glavnaya1">${ginmenusen}</h3>
+
+    `
     sentings.style = "display:block;";
     lighting.style = "display:block;";
+    svet.style = "display:none;";
+    temn.style = "display:none;";
     menuafterclick.style = "display:none;";
     afterlogin.style = "display: none;";
     
 });
 
 lighting.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginmenu}</h3>
+        <svg class="arrowto9" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  d="M0.739665 0.200408C0.436132 0.48226 0.418556 0.956809 0.700408 1.26034L4.22652 5L0.700408 8.73967C0.418556 9.0432 0.436132 9.51775 0.739665 9.7996C1.0432 10.0815 1.51775 10.0639 1.7996 9.76034L5.7996 5.51034C6.0668 5.22258 6.0668 4.77743 5.7996 4.48966L1.7996 0.239665C1.51775 -0.0638681 1.0432 -0.081444 0.739665 0.200408Z" fill="#323232"></path>
+        </svg>
+        <h3 class="dop-glavnaya1">${ginmenusen}</h3>
+        <svg class="arrowto10" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  d="M0.739665 0.200408C0.436132 0.48226 0.418556 0.956809 0.700408 1.26034L4.22652 5L0.700408 8.73967C0.418556 9.0432 0.436132 9.51775 0.739665 9.7996C1.0432 10.0815 1.51775 10.0639 1.7996 9.76034L5.7996 5.51034C6.0668 5.22258 6.0668 4.77743 5.7996 4.48966L1.7996 0.239665C1.51775 -0.0638681 1.0432 -0.081444 0.739665 0.200408Z" fill="#323232"></path>
+        </svg>
+        <h3 class="dop-glavnaya2">${ginmenulig}</h3>
+
+    `
     temn.style = "display:block;";
     svet.style = "display:block;";
     lighting.style = "display:none;";
@@ -161,6 +253,8 @@ svet.addEventListener('click', function(){
     header.style = "background-color: rgb(252, 252, 252);";
     foothead.style = ": background-color: rgb(252, 252, 252);";
     topbtn.style = "background-color:rgb(49, 48, 45); color:white; ";
+    ktomititle.style = "background-color: whitesmoke;color: #000000;display:flex;";
+    aboutcompany.style = "background-color: whitesmoke;color: #000000;";
     // vibor1.style = "color: #FF0000;";
     // vibor2.style = "color: black;";
     // vibor3.style = "color: black;";
@@ -174,6 +268,8 @@ temn.addEventListener('click', function(){
     header.style = "background-color: #c9c9c9;";
     foothead.style = "background-color: #c9c9c9;";
     topbtn.style = "background-color: white; color:rgb(49, 48, 45);";
+    ktomititle.style = "background-color: grey;color: white ;display:flex;";
+    aboutcompany.style = "background-color: grey;color: white;";
     // vibor1.style = "color: white;";
     // vibor2.style = "color: white;";
     // vibor3.style = "color: white;";
@@ -182,6 +278,14 @@ temn.addEventListener('click', function(){
     // header.style = "border-bottom: 1px solid white;";
 });
 btn2.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginbtn2}</h3>
+    `
+    topbtn.style = "background-color:rgb(49, 48, 45); color:white; ";
+    ktomititle.style = "background-color: whitesmoke;color: #000000;display:flex;";
+    aboutcompany.style = "background-color: whitesmoke;color: #000000;";
+    main1.style = "display:none;";
     afterlogin.style = "display: block;";
     menuafterclick.style = "display: none;";
     sentings.style = "display: none;";
@@ -193,8 +297,14 @@ btn2.addEventListener('click', function(){
     afterclickprice.style = "display:none;";
     container.style = "display:block;";
     body.style = "background-color: #00000021;";
+    btninput.style = "display:none;";
 });
 otmena.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya"></h3>
+    `
+    main1.style = "display:none;";
     afterlogin.style = "display: none;";
     container.style = "display:none;";
     body.style = "background-color: white;";
@@ -202,14 +312,27 @@ otmena.addEventListener('click', function(){
     input2.value = "";
 }); 
 menu.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginmenu}</h3>
+        <svg class="arrowto9" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  d="M0.739665 0.200408C0.436132 0.48226 0.418556 0.956809 0.700408 1.26034L4.22652 5L0.700408 8.73967C0.418556 9.0432 0.436132 9.51775 0.739665 9.7996C1.0432 10.0815 1.51775 10.0639 1.7996 9.76034L5.7996 5.51034C6.0668 5.22258 6.0668 4.77743 5.7996 4.48966L1.7996 0.239665C1.51775 -0.0638681 1.0432 -0.081444 0.739665 0.200408Z" fill="#323232"></path>
+        </svg>
+    `
+    main1.style = "display:none;";
     menu1.style = "display:block;";
     menu.style = "display:none;";
     menu1.style = "display:flex;";
     menuafterclick.style = "display:block;";
     afterlogin.style = "display: none;";
     afterclickprice.style = "display: none;";
+    btninput.style = "display:none;";
 }); 
 menu1.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya"></h3>
+    `
     menu.style = "display:block;";
     menu1.style = "display:none;";
     menu.style = "display:flex;";
@@ -218,8 +341,13 @@ menu1.addEventListener('click', function(){
     svet.style = "display:none;";
     temn.style = "display:none;";
     afterclickreview.style = "display:none;";
+    btninput.style = "display:none;";
 }); 
 lan.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginlan}</h3>
+    `
     input1.value = "";
     input2.value = "";
     lan.style = "display: none;";
@@ -242,8 +370,13 @@ lan.addEventListener('click', function(){
     daxil.innerHTML = 'Daxil ol';
     noakk.innerHTML = 'Hesabınız yoxdur?';
     reges.innerHTML = 'Qeydiyyatdan keçin';
+    btninput.style = "display:none;";
 });
 lan1.addEventListener('click', function(){
+    dopgl.innerHTML =
+    `
+        <h3 class="dop-glavnaya">${ginlan1}</h3>
+    `
     input1.value = "";
     input2.value = "";
     lan.style = "display: block;";
@@ -265,4 +398,5 @@ lan1.addEventListener('click', function(){
     daxil.innerHTML = 'Войти';
     noakk.innerHTML = 'Нет аккаунта?';
     reges.innerHTML = 'Зарегистрируйтусь';
+    btninput.style = "display:none;";
 });
