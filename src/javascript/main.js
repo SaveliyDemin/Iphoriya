be3df44166e8508b89c0dc15ed266a51f5fe10ca
SaveliyDemin <deminalal@yandex.ru>
2024-06-14 13:32:22 +0400

    1.0

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,26 +1,40 @@
 const btnsearch = document.querySelector(".btnsearch");
 const btnsearchxmark = document.querySelector(".btnsearchxmark");
 const btnmenu = document.querySelector(".btnmenu");
+const btnmenuxmark = document.querySelector(".btnmenuxmark");
 const btnshop = document.querySelector(".btnshop");
 const afterclickbtnsearch = document.querySelector(".afterclickbtnsearch");
 const infarmation = document.querySelector(".infarmation");
 const search = document.querySelector("input");
+const headpart3 = document.querySelector(".headpart3");
 
 
 btnsearch.addEventListener('click', function(){
-    btnsearchxmark.style = "display:block;"
-    afterclickbtnsearch.style = "display:block;display:flex;"
-    infarmation.style = "display:none;"
-    btnsearch.style = "display:none;"
+    btnsearchxmark.style = "display:block;";
+    afterclickbtnsearch.style = "display:block;display:flex;";
+    infarmation.style = "display:none;";
+    btnsearch.style = "display:none;";
 
 });
 btnsearchxmark.addEventListener('click', function(){
-    btnsearchxmark.style = "display:none;"
-    btnsearch.style = "display:block;"
-    afterclickbtnsearch.style = "display:none;"
-    infarmation.style = "display:block;display:flex;"
+    btnsearchxmark.style = "display:none;";
+    btnsearch.style = "display:block;";
+    afterclickbtnsearch.style = "display:none;";
+    infarmation.style = "display:block;display:flex;";
     search.value = "";
 });
+btnmenu.addEventListener('click', function(){
+    btnmenuxmark.style = "display:block;";
+    headpart3.style = "display:flex;";
+    btnmenu.style = "display:none;";
+
+});
+btnmenuxmark.addEventListener('click', function(){
+    btnmenu.style = "display:block;";
+    btnmenuxmark.style = "display:none;";
+    headpart3.style = "display:block;display:none !important;";
+});
+
 
 topbtn.addEventListener('click', tophand );
 function tophand(){
